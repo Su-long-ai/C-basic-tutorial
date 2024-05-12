58f773d3564d2bcd4af3f0ca42ab8b6d656e0648
Sulong <88147012+Su-long-ai@users.noreply.github.com>
2024-05-12 18:48:25 +0800

    Add files via upload

---
+++ C++ basic tutorial_2/point.cpp
@@ -0,0 +1,6 @@
+#include"point.h"
+//这里只要写函数就行
+//void Point::setx()    //要写作用域
+//{
+
+//}
---
+++ C++ basic tutorial_2/point.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class point
+{
+//这里函数只要写声明就行了
+public:
+
+	
+
+	point() = default;
+
+	bool operator==(const point& other) const
+	{
+		return false;
+	}
+};
